3f871de7ee06b36fb8a91f3855dfeab9b3eea585
rasc3ta <greenfair.shan5@gmail.com>
2024-09-21 00:05:07 +0600

    banner section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,18 @@ const Home = (props) => {
     <div className="max-w-[1280px] mx-auto">
       <div className="relative">
         <Carousel></Carousel>
-        <div className="absolute bottom-[10%] left-[0%] z-[2] bg-[#1D4ED880] p-3 px-5 ">
+        <div className="absolute top-[5%] left-[0%] z-[2] bg-[#1D4ED880] p-3 px-5 ">
           <h1 className="text-5xl lg:text-7xl text-white text-left">
             ISTT Unify
           </h1>
+
           <TypewriterComp></TypewriterComp>
+        </div>
+        <div>
+          <p className="absolute bottom-[10%] z-[2] text-xl md:text-2xl text-white text-left bg-[#1D4ED880] p-3 px-5 max-w-[500px]">
+            Discover profiles of talented students from, stay updated,
+             and build connections that matter.
+          </p>
         </div>
       </div>
     </div>
